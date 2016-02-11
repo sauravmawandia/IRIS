e60859d33b27a40e4a24de6a84097f855ac79b99
sauravmawandia <saurav.mawandia@in.tesco.com>
2016-02-11 19:13:32 +0530

    Removing unwanted files

--- services/getPoints.js
+++
@@ -1,6 +0,0 @@
-app.get('/getPoints/:clubcard', function (req, res, next) {
-  getPoints.findById(req.params.clubcard, function(err, getPoints){
-    if(err) res.send(err);
-    res.json(getPoints);
-  });
-});
